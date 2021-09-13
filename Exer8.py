34ec3994a673338e3232dfe064b3eea33bf237e8
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-13 16:41:31 -0300

    Create Exer8.py

---
+++ Exer8.py
@@ -0,0 +1,11 @@
+bebidas = input("Escolha uma bebida:\nA - àgua\nB - Refrigerante\nC - café\n:")
+if(bebidas == "A")or(bebidas == "a"):
+    print("Você escolheu àgua, boa escolha!")
+elif(bebidas == "B")or(bebidas == "b"):
+    print("Você escolheu refrigerante, boa escolha!")
+
+elif(bebidas == "C")or(bebidas == "c"):
+    print("Você escolheu café, boa escolha!")
+
+else:
+    print("Erro, letra não identificada...")
