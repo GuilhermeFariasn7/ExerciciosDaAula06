6df07a90c77b4b007ff0dbe9dd5026f05a9326f3
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-13 16:41:14 -0300

    Create Exer3.py

---
+++ Exer3.py
@@ -0,0 +1,9 @@
+sexo = input("Digite seu sexo(masculino ou feminino):")
+if(sexo == "masculino")or(sexo == "Masculino"):
+    print("Sexo M")
+
+elif(sexo == "feminino")or(sexo == "Feminino"):
+    print("Sexo F")
+
+else:
+    print("Sexo invalido!")
