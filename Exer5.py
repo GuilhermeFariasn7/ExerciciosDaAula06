08cbd149c3ffe9d2d8d783983513060962ba45a9
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-13 16:41:22 -0300

    Create Exer5.py

---
+++ Exer5.py
@@ -0,0 +1,7 @@
+escolha = input("Qual a capital é a capital de tocantins?(Digite a letra por favor!) \nA - Alagoas\nB-Tocantins\nC - Jerusalem\n:")
+if(escolha == "B" )or(escolha == "b" ):
+    print("Parabéns você acertou... Palmas...")
+else:
+    print("Que pena você errou, a alternativa correta era a B palmas!")
+
+    
