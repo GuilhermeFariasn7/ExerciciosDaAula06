0bb089dc76e282187d7ec847b5ac679ad0cb9bdb
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-13 16:41:08 -0300

    Create Exer1.py

---
+++ Exer1.py
@@ -0,0 +1,8 @@
+num1 = int(input("Digite um número real: "))
+num2 = int(input("Digite outro número real: "))
+
+if(num1>num2):
+    print(num1,"é o maior número!")
+
+else:
+    print(num2,"é o maior número!")
