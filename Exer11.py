84877b21616db0c7b7cc4c42702a87f3bf0fd437
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-13 16:44:10 -0300

    Create Exer11.py

---
+++ Exer11.py
@@ -0,0 +1,10 @@
+num1 = int(input("Insira um número real\n: "))
+num2 = int(input("Insira outro número real\n: "))
+
+escolha = int(input("que tipo de conta você quer fazer?\n1 - multiplicação\n2 - divisão\n:"))
+if(escolha == 1):
+    print("O resultado da multiplicação deu: ",(num1*num2))
+elif(escolha == 2):
+    print("O resultado da divisão deu: ",(num1-num2))
+else:
+    print("Erro...")
