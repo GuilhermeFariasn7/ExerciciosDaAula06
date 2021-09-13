5b8cde0468ad9df811f93e3638925ab81dd17dae
GuilhermeFariasn7 <88680855+GuilhermeFariasn7@users.noreply.github.com>
2021-09-13 16:41:28 -0300

    Create Exer7.py

---
+++ Exer7.py
@@ -0,0 +1,11 @@
+Pir = input("Você possui irmãos(Sim ou não)\n:")
+if(Pir=="Sim")or(Pir == "sim"):
+    int(input("Quantos irmãos você tem?\n:"))
+    print("Ok, até mais!")
+
+elif(Pir == "Não")or(Pir == "não"):
+    gostaria = input("Gostaria de ter?\n:")
+    if(gostaria =="Sim")or(gostaria == "sim" ):
+        print("Reveja suas ideias...")
+    else:
+        print("Ok.")
